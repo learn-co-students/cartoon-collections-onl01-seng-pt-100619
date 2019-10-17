1cb1c89691aaef6e6eec4823326772575e542152
allison cortez <allizetorc@gmail.com>
2019-10-17 20:10:38 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+call_dwarves= []
+  names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
+  # call_dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|name| name.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  long_calls = []
+  i = 0 
+  while i < calls.length 
+  long_calls << calls.include?(calls.length < 4 )
+  i += 1 
+end
+long_calls
 end
 
 def find_the_cheese# code an argument here
