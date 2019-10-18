462911824d78a1d0a44f60cbfbd1074f1eda8edb
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-18 15:48:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+  array.map.with_index do |name,index|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.map{|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_element)
+  array_element.any?{|element| element.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(item_list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  item_list.find do |item|
+    cheese_types.include?(item)
+  end
 end
