fa70b8720ed4f63d5e7903c10af7867adde88621
Ricky dildy <rdildy92@gmail.com>
2019-10-30 02:35:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,54 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves("Doc", "Dopey", "Bashful", "Grumpy")
+  roll_call_dwarves.index |dwarves|
+  puts dwarves.index 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  while summon_captain_planet.map |planeteer_calls|
+    planeteer_calls.uppercase 
+    puts "Earth!", "Wind!", "Fire!", "Water!", "Heart!"
+    end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+short_words = ["puff", "go", "two"]
+def long_planeteer_calls(short_words)
+  if long_planeteer_calls(short_words) is false
+      assorted_words = ["two", "go", "industrious", "bop"]
+       then long_planeteer_calls(assorted_words)
+           #true
+         end 
+     end 
+end 
+
+snacks = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese(snacks)
+ cheese_types = ["cheddar", "gouda", "camembert"]
+  find_the_cheese(snacks).collect |cheese_types|
+  #expect find_the_cheese == ("gouda")
+  
 end
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+def find_the_cheese(soup)
+  find_the_cheese(soup).map |cheese_types|
+   if (soup) = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+     #true 
+      then find_the_cheese 
+      puts "cheddar, gouda"
+       end 
+     end 
+   end 
+ end 
+  
+ingredients = ["garlic", "rosemary", "bread"]
+def find_the_cheese(ingredients)
+  if ingredients.include |cheese_types|
+    then statment is true 
+     end 
+   end 
+ end 
